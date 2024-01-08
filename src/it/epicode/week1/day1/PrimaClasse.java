1899121e74d63bdfc028c7f19966488e1b7f26b5
Fabio Caldarini <fabio.caldarini99@gmail.com>
2024-01-08 18:14:23 +0300

    Initial commit

---
+++ src/it/epicode/week1/day1/PrimaClasse.java
@@ -0,0 +1,29 @@
+package it.epicode.week1.day1;
+
+public class PrimaClasse {
+    public static void main(String[]args){
+        double multi = multi(10,20);
+        System.out.println(multi);
+    }
+
+
+    public static double multi(double x, double y){
+        return x*y;
+    }
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
