d6b1ae5f3f17146897c861e418dcac966d447dc0
Fabio Caldarini <fabio.caldarini99@gmail.com>
2024-01-11 18:41:00 +0300

    esercizio giorno 4

--- src/it/epicode/week1/day4/CalcoliDipendente.java
+++ src/it/epicode/week1/day4/CalcoliDipendente.java
@@ -4,9 +4,9 @@ import java.util.Scanner;
 
 public class CalcoliDipendente {
     public static void main(String[] args) {
-        Dipendente dipendente = new Dipendente(1200.3,"123",1200,3.2,Livello.QUADRO,Dipartimento.PRODUZIONE);
-    dipendente.datiDipendente();
-    dipendente.promuovi();
-    dipendente.datiDipendente();
+        Dipendente dipendente = new Dipendente(1200.3, "123", 1200, 3.2, Livello.QUADRO, Dipartimento.PRODUZIONE);
+        dipendente.datiDipendente();
+        dipendente.promuovi();
+        dipendente.datiDipendente();
     }
 }
